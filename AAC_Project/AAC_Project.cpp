3d7763ff2355b6589230bc9bb255971317a4a0e9
foxdust42 <foxdust42@gmail.com>
2024-11-11 00:09:10 +0100

    Max Cycle Progress

--- AAC_Project/AAC_Project.cpp
+++ AAC_Project/AAC_Project.cpp
@@ -75,6 +75,100 @@ void printGraph(const Graph& graph) {
     }
 }
 
+Graph InitGraph(int size) {
+    Graph g;
+    g.vertices = size;
+    g.adjacencyMatrix = std::vector<std::vector<int> >(size, std::vector<int>(size));
+    return g;
+}
+
+Graph SubGraph(Graph * g, std::vector<int> mask, int * neighbours = NULL) {
+
+    int sub_size = g->vertices - mask.size();
+    Graph s = InitGraph(sub_size);
+
+    std::vector<int> n = std::vector<int>(mask.size(), 0);
+
+    int si = 0;
+    for (int i = 0; i < g->vertices; i++) // rows
+    {
+        int sj = 0;
+
+        if (i == mask[i - si]) { // masked row
+            for (int j = 0; j < g->vertices; j++)
+            {
+                if (j != mask[j - sj]) { // masked row, unmasked column
+                    n[i - si] += g->adjacencyMatrix[i][j] >= 1 ? 1 : 0;
+                    sj ++;
+                }
+                //masked row, masked column
+                //nop;
+            }
+            continue;
+        }
+
+        //unmasked row
+        for (int j = 0; j < g->vertices; j++) // cols
+        {
+            if (j == mask[j - sj]){ // unmasked row, masked column
+                if (g->adjacencyMatrix[i][j] >= 1) {
+                    n[j - sj]++;
+                }
+                continue;
+            }
+            //unmasked row, unmasked column
+            s.adjacencyMatrix[si][sj] = g->adjacencyMatrix[i][j];
+            sj ++;
+        }
+        si ++;
+    }
+    if (neighbours != NULL) {
+        *neighbours = 0;
+        for (int i = 0; i < (int)n.size(); i++)
+        {
+            *neighbours += n[i] > 0 ? 1 : 0;
+        }
+    }
+    return s;
+}
+
+std::vector<std::vector<int>> MatMul(std::vector<std::vector<int>> mat1, std::vector<std::vector<int>> mat2) {
+    // assumes the sizes are equal
+    std::vector<std::vector<int>> res = std::vector<std::vector<int> >(mat1.size(), std::vector<int>(mat1.size(), 0));
+
+    for (int i = 0; i < mat1.size(); i++)
+    {
+        for (int j = 0; j < mat1.size(); j++)
+        {
+            for (int k = 0; k < mat1.size(); k++)
+            {
+                res[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+    return res;
+}
+
+int binom(int n, int k) {
+    if (n < 0 || k < 0 || k > n){
+        return 0;
+    }
+    int p = 1;
+    for (int i = 1; i <= std::min(k, n-k); i++)
+    {
+        p *= n + 1 - i; 
+        p /= i;
+    }
+    return p;
+}
+
+int MaxCycle(Graph * g, int * no_cycles = NULL) {
+    // based on https://arxiv.org/pdf/1612.05531
+    // NOTE:: this returnx the number of **directed** cycles. so a singular cycle in an undirected graph will count for two
+
+
+}
+
 int main() {
     string filename;
     cout << "Data file path: ";
@@ -86,7 +180,15 @@ int main() {
     for (size_t i = 0; i < graphs.size(); ++i) {
         cout << "\nGraph " << i + 1 << ":" << endl;
         printGraph(graphs[i]);
+        cout << "\nSubgraph:\n";
+        int n = 0;
+        Graph sub = SubGraph(&graphs[0], {3}, &n);
+        printGraph(sub);
+        cout << "neighbours: " << n << '\n';
     }
+
+
+
 
     return 0;
 }
