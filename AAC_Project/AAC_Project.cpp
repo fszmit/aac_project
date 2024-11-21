ce8241dd26cd073e5ff7941148d9cf04d77d0bf6
Filip Szmit <fifurszmit@gmail.com>
2024-11-22 00:40:09 +0100

    Added Edit distnace functionality for directed/undirected graphs. Apply intial UI for user.

--- AAC_Project/AAC_Project.cpp
+++ AAC_Project/AAC_Project.cpp
@@ -332,6 +332,162 @@ int MaxCycle(Graph * g, int * no_cycles = NULL) {
     *no_cycles = 0;
     return 0;
 }
+int GraphSize(const Graph& graph) {
+    int EdgeSum = 0;
+    if (graph.additionalInfo.compare("directed") != string::npos) {
+        for (int i = 0; i < graph.vertices; i++) {
+            for (int j = 0; j < graph.vertices; j++) {
+                EdgeSum += graph.adjacencyMatrix.at(i).at(j);
+            }
+        }
+    }
+    else {
+        for (int i = 0; i < graph.vertices; i++) {
+            for (int j = i; j < graph.vertices; j++) {
+                EdgeSum += graph.adjacencyMatrix.at(i).at(j);
+            }
+        }
+    }
+    return EdgeSum;
+}
+// assuming both graphs are direct/undirect
+int CalculateGraphEditDistance(Graph& A, Graph& B) {
+    int distance = 0;
+    distance += abs(B.vertices - A.vertices);
+    int bigger = B.vertices;
+    int smaller = A.vertices;
+    if (B.vertices < A.vertices) {
+        bigger = A.vertices;
+        smaller = B.vertices;
+    }
+    if (A.additionalInfo == "directed") {
+        for (int i = 0; i < bigger; i++) {
+            for (int j = 0; j < bigger; j++) {
+                if (i >= A.vertices || j >= A.vertices) {
+                    distance += B.adjacencyMatrix.at(i).at(j);
+                }
+                else if(i >= B.vertices || j >= B.vertices) {
+                    distance += A.adjacencyMatrix.at(i).at(j);
+                }
+                else
+                    distance += abs(A.adjacencyMatrix.at(i).at(j) - B.adjacencyMatrix.at(i).at(j));
+            }
+        }
+    }
+    else {
+        for (int i = 0; i < bigger; i++) {
+            for (int j = i; j < bigger; j++) {
+                if (i >= A.vertices || j >= A.vertices) {
+                    distance += B.adjacencyMatrix.at(i).at(j);
+                }
+                else if (i >= B.vertices || j >= B.vertices) {
+                    distance += A.adjacencyMatrix.at(i).at(j);
+                }
+                else
+                    distance += abs(A.adjacencyMatrix.at(i).at(j) - B.adjacencyMatrix.at(i).at(j));
+            }
+        }
+    }
+    return distance;
+}
+
+
+void UserInterace(vector<Graph>& graphs) {
+    int option = 0;
+    int i = 0;
+    int size = graphs.size();
+    while (true) {
+        // Display menu
+        cout << "\n=== Graph Operations Menu ===\n";
+        cout << "1. Show Size of Graph\n";
+        cout << "2. Calculate Distance Between Graphs\n";
+        cout << "3. Find Maximal Cycle in a Graph\n";
+        cout << "4. Minimal Extension of a Graph for Hamiltonian Cycle\n";
+        cout << "5. Show All Graphs\n";
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+
+        // Get user option
+        cin >> option;
+
+        // Handle invalid input
+        if (cin.fail() || option < 0 || option > 5) {
+            cin.clear(); // Clear the error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore invalid input
+            cout << "Invalid choice. Please try again.\n";
+            continue;
+        }
+
+        // Process based on option
+        switch (option) {
+        case 1: {
+            int graphNumber;
+            cout << "Enter the graph number to show size: ";
+            cin >> graphNumber;
+            if (graphNumber >= size || graphNumber < 0) {
+                cout << "No such graph!";
+                continue;
+            }
+            cout << "Size of Graph " << GraphSize(graphs.at(graphNumber)) << ".\n";
+            break;
+        }
+        case 2: {
+            int graph1, graph2;
+            cout << "Enter the first graph number: ";
+            cin >> graph1;
+            if (graph1 >= size || graph1 < 0) {
+                cout << "No such graph!";
+                continue;
+            }
+            cout << "Enter the second graph number: ";
+            cin >> graph2;
+            if (graph2 >= size || graph2 < 0) {
+                cout << "No such graph!";
+                continue;
+            }
+            cout << "Edit distance between Graph " << graph1 << " and Graph " << graph2 << " is: " << CalculateGraphEditDistance(graphs.at(graph1), graphs.at(graph2)) << endl;
+            break;
+        }
+        case 3: {
+            int graphNumber;
+            cout << "Enter the graph number to find maximal cycles: ";
+            cin >> graphNumber;
+            if (graphNumber >= size || graphNumber < 0) {
+                cout << "No such graph!";
+                continue;
+            }
+            cout << "You chose to find maximal cycles in Graph " << graphNumber << ".\n";
+            break;
+        }
+        case 4: {
+            int graphNumber;
+            cout << "Enter the graph number for Hamiltonian cycle operations: ";
+            cin >> graphNumber;
+            if (graphNumber >= size || graphNumber < 0) {
+                cout << "No such graph!";
+                continue;
+            }
+            cout << "You chose to find minimal extension and number of Hamiltonian cycles for Graph "
+                << graphNumber << ".\n";
+            break;
+        }
+        case 5:
+            
+            cout << "You chose to display all graphs.\n";
+            for (Graph g : graphs) {
+                cout << "[" << i << "]" << endl;
+                printGraph(g);
+                i++;
+            }
+            break;
+        case 0:
+            cout << "Exiting program. Goodbye!\n";
+            return;
+        default:
+            cout << "Invalid option. Please try again.\n";
+        }
+    }
+}
 
 int main() {
     string filename;
@@ -342,7 +498,9 @@ int main() {
     vector<Graph> graphs = parseGraphs(filename);
     cout << "Parsed " << graphs.size() << " graphs." << endl;
 
-    for (size_t i = 0; i < graphs.size(); ++i) {
+    UserInterace(graphs);
+
+    /*for (size_t i = 0; i < graphs.size(); ++i) {
         cout << "\nGraph " << i + 1 << ":" << endl;
         printGraph(graphs[i]);
         cout << "\nSubgraph:\n";
@@ -361,8 +519,9 @@ int main() {
         cout << "Max Cycle size: " << MaxCycle(&graphs[i], &no_cycles) << '\n';
         cout << "No Max Cycles: " << no_cycles << '\n';
 
-        cout << "\n\n";
+        cout << "\n\n";   
     }
+    */
 
 
     return 0;
